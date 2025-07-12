02320b4dffe61ebd81441c3c2652572b39aac27f
etec10 <etec10@gmail.com>
2025-07-12 14:18:37 -0300

    Delete trader.js

--- trader.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-script.type = 'module';
-script.crossOrigin = 'anonymous'; // or 'use-credentials' if needed
-script.src = 'https://cdn.drv.pro/assets/index-CbpbrPsO.js';
-document.body.appendChild(script);
