181592ba1649dad194467d7f812ec750bc231231
etec10 <etec10@gmail.com>
2025-07-12 09:23:43 -0300

    Add files via upload

---
+++ trader.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.type = 'module';
+script.crossOrigin = 'anonymous'; // or 'use-credentials' if needed
+script.src = 'https://cdn.drv.pro/assets/index-CbpbrPsO.js';
+document.body.appendChild(script);
