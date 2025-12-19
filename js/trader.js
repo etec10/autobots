d3386e1768b925dd06bec645281977e70b90e00a
etec10 <etec10@gmail.com>
2025-12-19 12:41:27 -0300

    Update trader.js

--- js/trader.js
+++ js/trader.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 script.type = 'module';
 script.crossOrigin = 'anonymous'; // or 'use-credentials' if needed
-script.src = 'https://cdn.drv.pro/trader/assets/index-DnkC_Vui.js';
+script.src = 'https://cdn.drv.pro/trader/assets/index-BEGt_9bs.js';
 document.body.appendChild(script);
